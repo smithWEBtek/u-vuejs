070a84654b54779db8f6cd7d39b0de5a31982eaa
smithwebtek <brad@smithwebtek.com>
2019-10-13 18:29:12 -0400

    Thru exercise 4

--- 32-excersize4-brad/app.js
+++ 32-excersize4-brad/app.js
@@ -2,13 +2,20 @@ new Vue({
   el: ' #exercise',
   data: {
     currentClass: 'highlight',
-    inputClassName: 'type from list'
+    inputClassName: 'type from list',
+    userInputClassName: null,
+    width: 50
   },
   methods: {
     startEffect: function () {
       this.currentClass === 'highlight' ? this.currentClass = 'shrink' : this.currentClass = 'highlight';
     },
-    stopEffect: function () { this.currentClass = 'highlight' }
+    stopEffect: function () {
+      this.currentClass = 'highlight'
+    },
+    startProgressBar: function () {
+      this.width += 20
+    }
   },
   watch: {
     currentClass: function () {
@@ -17,6 +24,15 @@ new Vue({
         vm.currentClass === 'highlight' ? vm.currentClass = 'shrink' :
           vm.currentClass = 'highlight'
       }, 3000);
+    }
+  },
+  computed: {
+    myStyle: function () {
+      let vm = this
+      return {
+        backgroundColor: this.color,
+        width: this.progressBar.width += 10 + 'px'
+      }
     }
   }
 });
