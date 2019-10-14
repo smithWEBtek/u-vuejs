9e1856511801d16a3ec9778dfcbb48c3fe82354e
smithwebtek <brad@smithwebtek.com>
2019-10-14 10:34:46 -0400

    Complete monster game project

--- section4-project-monster-slayer/47-62-project-videos/app.js
+++ section4-project-monster-slayer/47-62-project-videos/app.js
@@ -1,25 +1,104 @@
 new Vue({
   el: '#app',
   data: {
-    playerHealth: 40,
-    monsterHealth: 100,
-    gameIsRunning: false
+    playerHealth: 30,
+    monsterHealth: 10,
+    showPlayerAttackImage: false,
+    showMonsterAttackImage: false,
+    gameIsRunning: false,
+    turns: [],
+    isPlayer: false
+  },
+  watch: {
+    turns: function () {
+      console.log('watching ...')
+
+      var vm = this;
+      setTimeout(function () {
+        vm.counter = 0;
+      }, 2000);
+    }
   },
   methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.turns = [];
+    },
     attack: function () {
-      // lower the monsterHealthScore
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage + ' points.'
+      })
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttack();
+      this.checkWin();
     },
     specialAttack: function () {
-      // lower the monsterHealthScore
-      // increase myHealthScore
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster HARD for ' + damage + ' points.'
+      })
+
+      this.checkWin();
+      this.monsterAttack();
+    },
+    monsterAttack: function () {
+      var vm = this;
+      var damage = this.calculateDamage(5, 12)
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage + ' points.'
+      })
+
+      setTimeout(function () {
+        vm.playerHealth -= damage;
+      }, 1000)
     },
     heal: function () {
-      // increase myHealthScore
+      if (this.playerHealth <= 90) {
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10 points.'
+      })
+      this.monsterAttack();
     },
     giveUp: function () {
-      // reset monsterHeathscore to 0
-      // reset myHeathscore to 0
-      // prompt to restart game
+      this.gameIsRunning = false;
     },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lose! New game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min)
+    }
   }
 });
